43157cf382a553136bfc52e08b81aa72d9e66184
Jan Jadud <dev@gojadud.com>
2020-03-25 18:56:22 +0100

    Restructure 1

--- gulp_tasks/clean.js
+++
@@ -1,9 +0,0 @@
-import del from 'del';
-import PATHS from './../paths';
-
-function clean(done) {
-  console.log('running: Clean');
-  return del([PATHS.APP.dist], done);
-}
-
-export default clean;
